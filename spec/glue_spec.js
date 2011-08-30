23e6da5a481d2b8ee64bf2df966f2c7a563f2a2b
Felix Flores <felix@edgecase.com>
2011-08-30 00:06:29 -0400

    Add specs for get and set; Move get and set from object controller into glue; Turn get and set into atomic operations.

--- spec/glue_spec.js
+++ spec/glue_spec.js
@@ -6,27 +6,59 @@ var vows = require('vows')
 ,   Glue = require("../lib/glue");
 
 suite.addBatch({
-  "setPropertyOnBoundObject": {
+  "set": {
+
     "simple assignment": function() {
       var topic = new Glue({level1: ''});
 
-      topic.setPropertyOnBoundObject('level1', 'top level');
+      topic.set('level1', 'top level');
       assert.equal(topic.boundObject.level1, "top level");
     },
 
-    "singly nested assignment": function() {
+    "nested assignment": function() {
       var topic = new Glue({level1: {level2: ''}});
 
-      topic.setPropertyOnBoundObject('level1.level2', 'two levels');
+      topic.set('level1.level2', 'two levels');
       assert.equal(topic.boundObject.level1.level2, "two levels");
-    },
-
-    "doubly nested assignment": function() {
-      var topic = new Glue({level1: {level2: {level3: ''}}});
-
-      topic.setPropertyOnBoundObject('level1.level2.level3', 'three levels');
-      assert.equal(topic.boundObject.level1.level2.level3, "three levels");
     }
   },
-}).export(module);
+});
+
+suite.addBatch({
+  "get": {
+
+    "a singly nested property": function(topic) {
+      var topic = new Glue({foo: 'bar'});
+
+      assert.equal(topic.get("foo"), "bar");
+    },
+
+    "a doubly nested property": function(topic) {
+      var topic = new Glue({foo: {bar: 'baz'}});
+
+      assert.equal(topic.get("foo.bar"), "baz");
+    }
+  }
+});
+
+
+suite.addBatch({
+  "getBoundObject": {
+    topic: new Glue({foo: 1}),
+
+    "returns a copy of the bound object": function(topic) {
+      assert.deepEqual(topic.getBoundObject(), {foo: 1});
+    },
+
+    "manipulating object returned should not returned the actual bound object": function(topic) {
+      var boundObject = topic.getBoundObject();
+
+      boundObject.foo = 2;
+      assert.deepEqual(topic.boundObject, {foo: 1});
+      assert.notDeepEqual(topic.boundObject, {foo: 2});
+    }
+  }
+});
+
+suite.export(module);
 
