2c436d344de61d92630316cbe701adfb0b99a34e
Felix Flores <felix@edgecase.com>
2011-08-30 20:25:21 -0400

    Refactored glue.js basic addObserver function

--- spec/glue_spec.js
+++ spec/glue_spec.js
@@ -7,19 +7,30 @@ var vows = require('vows')
 
 suite.addBatch({
   "set": {
+    topic: new Glue({level1: ''}),
 
-    "simple assignment": function() {
-      var topic = new Glue({level1: ''});
-
+    "simple assignment": function(topic) {
       topic.set('level1', 'top level');
-      assert.equal(topic.boundObject.level1, "top level");
+      assert.equal(topic.getBoundObject().level1, "top level");
     },
 
     "nested assignment": function() {
       var topic = new Glue({level1: {level2: ''}});
 
       topic.set('level1.level2', 'two levels');
-      assert.equal(topic.boundObject.level1.level2, "two levels");
+      assert.equal(topic.getBoundObject().level1.level2, "two levels");
+    },
+
+    "deeply nested assignment": function() {
+      var topic = new Glue({level1: {level2: {level3: ''}}});
+
+      topic.set('level1.level2.level3', 'three levels');
+      assert.equal(topic.getBoundObject().level1.level2.level3, "three levels");
+    },
+
+    "invocation returns itself for chainability": function(topic) {
+      var returnedValue = topic.set('level1', 'top level');
+      assert.equal(topic, returnedValue);
     }
   },
 });
@@ -27,20 +38,48 @@ suite.addBatch({
 suite.addBatch({
   "get": {
 
-    "a singly nested property": function(topic) {
+    "a property": function(topic) {
       var topic = new Glue({foo: 'bar'});
 
       assert.equal(topic.get("foo"), "bar");
     },
 
-    "a doubly nested property": function(topic) {
+    "a nested property": function(topic) {
       var topic = new Glue({foo: {bar: 'baz'}});
 
       assert.equal(topic.get("foo.bar"), "baz");
+    },
+
+    "a deeply property": function(topic) {
+      var topic = new Glue({foo: {bar: {baz: 'zap'}}});
+
+      assert.equal(topic.get("foo.bar.baz"), "zap");
     }
   }
 });
 
+suite.addBatch({
+  "get with calculated keypath": {
+
+    "a calculated property": function(topic) {
+      var topic = new Glue({foo: 'bar'});
+
+      assert.equal(topic.get("(foo)"), "bar");
+    },
+
+    "a nested calculated property": function(topic) {
+      var topic = new Glue({foo: {bar: 'baz'}});
+
+      assert.equal(topic.get("foo.(bar)"), "baz");
+    },
+
+    "a chained calculated property": function(topic) {
+      var topic = new Glue({foo: {bar: 'baz'}});
+
+      assert.equal(topic.get("(foo).bar"), "baz");
+    }
+  }
+});
 
 suite.addBatch({
   "getBoundObject": {
@@ -60,5 +99,103 @@ suite.addBatch({
   }
 });
 
-suite.export(module);
+suite.addBatch({
+  "addObserver": {
+    topic: new Glue({foo: "bar", baz: "zap"}),
 
+    "can be assigned to an anonymous function": function(topic) {
+      var listenerHollaBackWasInvoked = false;
+
+      topic.addObserver(function() {
+        listenerHollaBackWasInvoked = true;
+      });
+
+      topic.set("foo", "baz");
+      assert.equal(listenerHollaBackWasInvoked, true);
+    },
+
+    "can be assigned to an object": function(topic) {
+      var listenerHollaBackWasInvoked = false;
+
+      topic.addObserver({an: 'object'}, function() {
+        listenerHollaBackWasInvoked = true;
+      });
+
+      topic.set("baz", "baz");
+      assert.equal(listenerHollaBackWasInvoked, true);
+    },
+
+    "can be assigned to an anonymous function with a keypath": function(topic) {
+      var listenerHollaBackWasInvoked = false;
+
+      topic.addObserver(function() {
+        listenerHollaBackWasInvoked = true;
+      }, 'foo');
+
+      topic.set("baz", "baz");
+      assert.equal(listenerHollaBackWasInvoked, false);
+
+      topic.set("foo", "baz");
+      assert.equal(listenerHollaBackWasInvoked, true);
+    },
+
+    "can be assigned to an object with a keypath": function(topic) {
+      var listenerHollaBackWasInvoked = false;
+
+      topic.addObserver({an: 'object'}, 'foo', function() {
+        listenerHollaBackWasInvoked = true;
+      });
+
+      topic.set("baz", "baz");
+      assert.equal(listenerHollaBackWasInvoked, false);
+
+      topic.set("foo", "baz");
+      assert.equal(listenerHollaBackWasInvoked, true);
+    }
+  }
+});
+
+
+// suite.addBatch({
+//   "addObserver with calculated and funtion keypath": {
+//     topic: new Glue({
+//       internalArray: [],
+// 
+//       foo: this.internalArray.length,
+// 
+//       bar: function() {
+//         return this.internalArray.length;
+//       }
+//     }),
+// 
+//     "can specify that a keypath is calculated": function(topic) {
+//       var listenerHollaBackWasInvoked = false;
+// 
+//       topic.addObserver(myObj, '(foo)', function() {
+//         listenerHollaBackWasInvoked = true;
+//       });
+// 
+//       topic.set('internalArray' = []);
+//       assert.equal(listenerHollaBackWasInvoked, false);
+// 
+//       topic.set('internalArray' = [3]);
+//       assert.equal(listenerHollaBackWasInvoked, true);
+//     },
+// 
+//     "can specify that a keypath is a function": function(topic) {
+//       var listenerHollaBackWasInvoked = false;
+// 
+//       topic.addObserver(myObj, 'foo()', function() {
+//         listenerHollaBackWasInvoked = true;
+//       });
+// 
+//       topic.set('internalArray' = []);
+//       assert.equal(listenerHollaBackWasInvoked, false);
+// 
+//       topic.set('internalArray' = [3]);
+//       assert.equal(listenerHollaBackWasInvoked, true);
+//     }
+//   }
+// });
+
+suite.export(module);
