81c44b98322f9aef735087afa1e0b93e8b541b83
Felix Flores <felix@edgecase.com>
2011-09-05 01:02:40 -0400

    Refactoring simple assignment to call only the set function

--- spec/glue_spec.js
+++ spec/glue_spec.js
@@ -11,21 +11,21 @@ suite.addBatch({
 
     "simple assignment": function(topic) {
       topic.set('level1', 'top level');
-      assert.equal(topic.getBoundObject().level1, "top level");
+      assert.equal(topic.boundObject.level1, "top level");
     },
 
     "nested assignment": function() {
       var topic = new Glue({level1: {level2: ''}});
 
       topic.set('level1.level2', 'two levels');
-      assert.equal(topic.getBoundObject().level1.level2, "two levels");
+      assert.equal(topic.boundObject.level1.level2, "two levels");
     },
 
     "deeply nested assignment": function() {
       var topic = new Glue({level1: {level2: {level3: ''}}});
 
       topic.set('level1.level2.level3', 'three levels');
-      assert.equal(topic.getBoundObject().level1.level2.level3, "three levels");
+      assert.equal(topic.boundObject.level1.level2.level3, "three levels");
     },
 
     "invocation returns itself for chainability": function(topic) {
