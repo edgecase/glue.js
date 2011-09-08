a2367f77da5f9bf874fab21c084ef28fb2f0ccfa
Felix Flores <felix@edgecase.com>
2011-09-08 18:43:52 -0400

    This test is stupid

--- spec/get_bound_object_spec.js
+++ spec/get_bound_object_spec.js
@@ -5,14 +5,10 @@ var vows = require('vows')
 ,   Glue = require("../lib/glue");
 
 suite.addBatch({
-  "checking boundObject safety": {
+  "": {
     topic: new Glue({foo: 1}),
 
     "returns a copy of the bound object": function(topic) {
-      assert.deepEqual(topic.getBoundObject(), {foo: 1});
-    },
-
-    "manipulating object returned should not returned the actual bound object": function(topic) {
       var boundObject = topic.getBoundObject();
 
       boundObject.foo = 2;
