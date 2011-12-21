87a3b4e1c80a8781572a52f673d645a84c7e27f0
Felix Flores <felix@edgecase.com>
2011-12-20 22:32:20 -0500

    tabula rasa

--- spec/bind_to_spec.js
+++ spec/bind_to_spec.js
@@ -5,41 +5,6 @@ var vows   = require('vows')
 var suite  = vows.describe('bindTo')
 
 suite.addBatch({
-  "ensures": {
-    topic: new Glue({}),
-
-    "that the target object of glue is changed": function(topic) {
-      topic.target = {};
-
-      topic.bindTo({an: "object"});
-
-      assert.notDeepEqual(topic.topic, {});
-      assert.deepEqual(topic.target, {an: "object"});
-    },
-
-    "notifies listeners with the old and new target object": function(topic) {
-      var message = {};
-
-      topic.target = {};
-
-      topic.addListener('target', function(msg) {
-        message = msg;
-      });
-
-      topic.bindTo({ an: "object" });
-
-      assert.deepEqual(message, {
-          operation: 'target'
-        , oldValue: {}
-        , newValue: { an: "object" }
-      });
-    },
-
-    "when invoked, returns itself for chainability": function(topic) {
-      var returnedValue = topic.addListener(function(){});
-      assert.equal(topic, returnedValue);
-    }
-  }
 });
 
 suite.export(module);
--- spec/get_spec.js
+++ spec/get_spec.js
@@ -2,45 +2,9 @@ var vows   = require('vows')
 ,   assert = require('assert')
 ,   Glue   = require(__dirname + "/../lib/glue");
 
-var suite  = vows.describe('get method');
+var suite  = vows.describe('get operation');
 
 suite.addBatch({
-  "assigned value": {
-    topic: new Glue({}),
-
-    "can be retrieved": function(topic) {
-      topic.target = { v1: 'get this' };
-      assert.equal(topic.get('v1'), 'get this');
-    },
-
-    "can be nested": function(topic) {
-      topic.target = { v1: { v2: 'get this' }};
-      assert.equal(topic.get('v1.v2'), 'get this');
-    }
-  },
-
-  "computed value": {
-    topic: new Glue({}),
-
-    "can be retrived": function(topic) {
-      topic.target = { arr: [1]};
-      assert.equal(topic.get('arr#length'), 1);
-    },
-
-    "can be nested": function(topic) {
-      topic.target = { v1: {arr: [1]} };
-      assert.equal(topic.get('v1.arr#length'), 1);
-    }
-  },
-
-  "normalizes keys": {
-    topic: new Glue({}),
-
-    "removes spaces on keys": function(topic) {
-      topic.target = { v1: { v2: 1 }};
-      assert.equal(topic.get('v1.         v2'), 1);
-    }
-  }
 });
 
 suite.export(module);
