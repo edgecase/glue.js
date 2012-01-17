69c146f00571850628db9a55394459c31692a563
Felix Flores <felix@edgecase.com>
2012-01-17 16:21:01 -0500

    Implement pop

--- spec/current_spec.js
+++ spec/current_spec.js
@@ -1,40 +1,30 @@
-var vows = require('vows')
-,   _ = require('underscore')
-,   assert = require('assert')
-,   Glue   = require(__dirname + "/../lib/glue")
-
-,   suite = vows.describe('current spec');
-
-suite.addBatch({
-  "current test": {
-    topic: new Glue ({ v1: { arr: [ { v2: '' } ]}}),
-
-    "notifies listener of that key": function(glue) {
-      var message = {};
-          original = { v1: { arr: [ { v2: '' } ]}},
-          current = { v1: { arr: [ { v2: 'a value' } ]}};
-
-      glue.addListener('v1.arr[]', function(msg) {
-        message = msg;
-      });
-
-      glue.notify('v1.arr[0].v2', 'set', original, current, { 'v1.arr[0].v2':
-        { value: { old: '', current: 'a value' }}
-      });
-
-      assert.deepEqual(message, {
-        oldValue: { v2: '' },
-        currentValue:  { v2: 'a value' },
-        operation: 'set',
-        key: 'v1.arr[0].v2',
-        changes: {
-            'v1.arr[0].v2': {
-                value: { current: 'a value', old: '' }
-            }
-        }
-      })
-    }
-  }
-});
-
-suite.export(module)
+// var vows = require('vows')
+// ,   assert = require('assert')
+// ,   Glue   = require(__dirname + "/../lib/glue")
+// 
+// ,   suite = vows.describe('current spec');
+// 
+// suite.addBatch({
+//   "current test": {
+//     topic: new Glue ({ v1: { arr: [ { v2: '' } ]}}),
+// 
+//     "notifies listener of that key": function(topic) {
+//       var message;
+// 
+//       topic.target = { arr: [] };
+// 
+//       topic.addListener('arr', function(msg) {
+//         message = msg;
+//       });
+// 
+//       topic.push('arr', 2);
+// 
+//       assert.deepEqual(message, {
+//         operation: "push",
+//         newValue: 2
+//       });
+//     }
+//   }
+// });
+// 
+// suite.export(module)
--- spec/pop_spec.js
+++ spec/pop_spec.js
@@ -2,9 +2,66 @@ var vows   = require('vows')
   , assert = require('assert')
   , Glue   = require(__dirname + "/../lib/glue")
 
-var suite  = vows.describe('pop operation');
+var suite  = vows.describe('pop');
 
 suite.addBatch({
+  "target is array": {
+    topic: new Glue([]),
+
+    "pops target object is no argument is passed": function(topic) {
+      topic.target = [1, 2];
+      topic.pop();
+      assert.deepEqual(topic.target, [1]);
+    },
+
+    "pops from arrays inside objects": function(topic) {
+      topic.target = {arr: [1, 2]};
+      topic.pop('arr');
+      assert.deepEqual(topic.target, {arr: [1]});
+    },
+
+    "pops from arrays in nested objects": function(topic) {
+      topic.target = {v1: { arr: [1, 2] }};
+      topic.pop('v1.arr');
+      assert.deepEqual(topic.target, {v1: {arr: [1]}});
+    },
+
+    "pops from arrays in nested objects": function(topic) {
+      topic.target = {arr1: {arr2: [1, 2] }};
+
+      topic.pop('arr1.arr2');
+      assert.deepEqual(topic.target, {arr1: {arr2: [1] }});
+    }
+  },
+
+  "notification": {
+    topic: new Glue([1, 2]),
+
+    "notifies with operation and oldValue": function(topic) {
+      var message;
+
+      topic.addListener(function(msg) {
+        message = msg;
+      });
+
+      topic.pop();
+      assert.deepEqual(message, {
+        oldValue: [ 1, 2 ],
+        currentValue: [ 1 ],
+        operation: 'pop'
+      });
+    }
+  },
+
+  "returns": {
+    topic: new Glue([]),
+
+    "popped value": function(topic) {
+      topic.target = [1, 2];
+      assert.deepEqual(topic.pop(), 2);
+    },
+  }
 });
 
 suite.export(module);
+
