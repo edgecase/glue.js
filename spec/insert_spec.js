673c2a727504135b1fbd3cac6e132541343ef746
Felix Flores <felix@edgecase.com>
2012-01-23 04:19:55 -0500

    Adjust insert to fit new api

--- spec/insert_spec.js
+++ spec/insert_spec.js
@@ -6,17 +6,15 @@ var suite  = vows.describe('insert operation');
 
 suite.addBatch({
   "target obj": {
-    topic: new Glue([]),
-
-    "inserts at a particular index": function(glue) {
-      glue.target = [1,2,3,4];
+    "inserts at a particular index": function() {
+      var glue =  new Glue([1,2,3,4]);
 
       glue.insert(1, 9);
       assert.deepEqual(glue.target, [1, 9, 2, 3, 4]);
     },
 
-    "inserts at a particular index within an a obj": function(glue) {
-      glue.target = { arr: [1,2,3,4] };
+    "inserts at a particular index within an a obj": function() {
+      var glue =  new Glue({ arr: [1,2,3,4] });
 
       glue.insert('arr', 1, 9);
       assert.deepEqual(glue.target, { arr: [1, 9, 2, 3, 4] });
@@ -24,85 +22,56 @@ suite.addBatch({
   },
 
   "notification": {
-    topic: new Glue([]),
-
-    "notifies from the point of insert": function(glue) {
-      var original = [],
-          current = [];
-
-      glue.target = [1, 2, 3, 4];
+    "notifies from the point of insert": function() {
+      var messages = [],
+          glue = new Glue([1, 2, 3, 4]);
 
       glue.addListener('[]', function(msg) {
-        original.push([msg.oldIndex, msg.oldValue]);
-        current.push([msg.currentIndex, msg.currentValue]);
+        messages.push(msg);
       });
 
       glue.insert(1, 9);
 
-      assert.deepEqual(original, [
-        [ undefined, undefined ],
-        [ 1, 2 ],
-        [ 2, 3 ],
-        [ 3, 4 ]
-      ]);
-
-      assert.deepEqual(current, [
-        [ 1, 9 ],
-        [ 2, 2 ],
-        [ 3, 3 ],
-        [ 4, 4 ]
+      assert.deepEqual(messages, [
+        { oldValue: 2, currentValue: 9, index: 1, operation: 'insert' },
+        { oldValue: 3, currentValue: 2, index: 2, operation: 'insert' },
+        { oldValue: 4, currentValue: 3, index: 3, operation: 'insert' },
+        { oldValue: undefined, currentValue: 4, index: 4, operation: 'insert' }
       ]);
     },
 
-    "top edgecase test": function(glue) {
-      var original = [],
-          current = [];
-
-      glue.target = [1, 2, 3, 4];
+    "top edgecase test": function() {
+      var messages = [],
+          glue = new Glue([1, 2, 3, 4]);
 
       glue.addListener('[]', function(msg) {
-        original.push([msg.oldIndex, msg.oldValue]);
-        current.push([msg.currentIndex, msg.currentValue]);
+        messages.push(msg);
       });
 
       glue.insert(0, 9);
 
-      assert.deepEqual(original, [
-        [ undefined, undefined ],
-        [ 0, 1 ],
-        [ 1, 2 ],
-        [ 2, 3 ],
-        [ 3, 4 ]
-      ]);
-
-      assert.deepEqual(current, [
-        [ 0, 9 ],
-        [ 1, 1 ],
-        [ 2, 2 ],
-        [ 3, 3 ],
-        [ 4, 4 ]
+      assert.deepEqual(messages, [
+        { oldValue: 1, currentValue: 9, index: 0, operation: 'insert' },
+        { oldValue: 2, currentValue: 1, index: 1, operation: 'insert' },
+        { oldValue: 3, currentValue: 2, index: 2, operation: 'insert' },
+        { oldValue: 4, currentValue: 3, index: 3, operation: 'insert' },
+        { oldValue: undefined, currentValue: 4, index: 4, operation: 'insert'
+    }
       ]);
     },
 
-    "end edgecase test": function(glue) {
-      var original = [],
-      current = [];
-
-      glue.target = [1, 2, 3, 4];
+    "end edgecase test": function() {
+      var messages = [],
+          glue = new Glue([1, 2, 3, 4]);
 
       glue.addListener('[]', function(msg) {
-        original.push([msg.oldIndex, msg.oldValue]);
-        current.push([msg.currentIndex, msg.currentValue]);
+        messages.push(msg);
       });
 
       glue.insert(4, 9);
 
-      assert.deepEqual(original, [
-        [ undefined, undefined ]
-      ]);
-
-      assert.deepEqual(current, [
-        [ 4, 9 ]
+      assert.deepEqual(messages, [
+        { oldValue: undefined, currentValue: 9, index: 4, operation: 'insert' }
       ]);
     }
   },
